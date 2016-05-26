8ef0ee6431f6360de67b85a185b6a09caf63089c
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-05-26 16:30:57 -0700

    (feat) Created savor angular module, savor controller, savor factory, and savor routing

---
+++ public/app.js
@@ -0,0 +1,55 @@
+var savor = angular.module('savor', ['ngRoute']);
+
+savor.config(function($routeProvider) {
+  $routeProvider
+
+  //route for the home page
+  .when('/', {
+    templateUrl: 'index.html',
+    controller: 'savorCtrl'
+  })
+})
+
+savor.controller('savorCtrl', ['$scope', function savorCtrl($scope) {
+  $scope.restaurant = {
+    name: '',
+    image: '',
+    username: '',
+    userPhoto: '',
+    foodRating: 0,
+    serviceRating: 0,
+    ambianceRating: 0,
+    recommendation: ''
+  };
+
+savor.factory('Restaurants' ['$http', function restaurantsFactory($http) {
+  var getRestaurants = function() {
+    return $http({
+      method: 'GET',
+      url: '/'
+    })
+    .then(function restaurantRetrieved(response) {
+      return response.data;
+    })
+    .catch(function(err) {
+      console.log('There was an error: ', err);
+    });
+  };
+
+  // var addRestaurant = function(request) {
+  //   return $http({
+  //     method: 'POST',
+  //     url: '/',
+  //     data: request
+  //   });
+  // };
+
+  return {
+    getRestaurants: getRestaurants
+    // addRestaurant: addRestaurant
+  };
+}]);
+
+
+
+}]);
