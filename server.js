c7ff53f403f3607039e02ffd4823f971a30626c9
Unobtainiumrock <unobtainiumrock@gmail.com>
2016-05-26 15:04:21 -0700

    "Add body parser url encoded. Change connect path to mongo database. Remove require path. Change console.log to reflect correct port listen."

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-// Might need path. It's a node thing.
-var path = require('path');
+
 
 // Create an instance of express
 var app = express();
 
 // Connect to mongo database
-mongoose.connect('mongodb://localhost/<insert angular.module name here>');
+mongoose.connect('mongodb://localhost/restaurant');
 
 
 // Add middleware. Note about express.static: this will be used in the event that we are jsut serving static file, such as a single page
 // application. This assumes that routing is being handled on the client side via angular.
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/../app'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Tell the server to listen to a port
-app.listen(process.env.PORT || 3000, function(){
-   console.log('Listening on port: 3000');
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening at port: ' + port);
 
 // Export our app apparently for testing/flexibility.
 // It was required by index.js files in a library fold in previous examples.
