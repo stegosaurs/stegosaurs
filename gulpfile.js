5ce82ecc17bf103880c93bb380a9bb736cf93bd2
gschrock <gabrielschrock@gmail.com>
2016-05-26 16:22:53 -0700

    added jshint and jshint-stylish for easier lint reading

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('default', function() {
-  console.log('I haz configured gulp');
+  console.log('Just type gulp in terminal: Gulp is functioning!');
 })
+
+gulp.task('lint', function() {
+  return gulp.src('public/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
