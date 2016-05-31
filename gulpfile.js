7bc4597b66377a5fdfad17c0a9123ef3fe71618c
gschrock <gabrielschrock@gmail.com>
2016-05-30 18:53:49 -0700

    Dependencies added for gulp-concat and gulp-uglify, need more work.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   console.log('Just type gulp in terminal: Gulp is functioning!');
@@ -12,3 +14,16 @@ gulp.task('lint', function() {
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('concat', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('lib/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
